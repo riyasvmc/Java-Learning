d314d3334e4733ce3ef982ec8be6ca8d553fa723
Riyas Valiyadan <riyas.vmc@gmail.com>
2020-10-23 10:53:55 +0530

    Initial commit

---
+++ src/basics/Regex.java
@@ -0,0 +1,21 @@
+package basics;
+
+import java.util.regex.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+/**
+ * A Regular expression is a string of characters that describes a character sequence.
+ *
+ * Created by Riyas Valiyadan on 23 Oct 2020
+ */
+
+// (1/2) Don't have to declare functions inside class.
+
+// (2/2) `main` does not require `args`, if your program doesn't require command-lines arguments.
+
+public class Regex {
+    public static void main(String[] args) {
+
+    }
+}
